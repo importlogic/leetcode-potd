dcc49a9d90dc0db5f5c3e429febf0a938ad8af15
Manas Rawat <manasrawat.cse@gmail.com>
2024-01-17 09:29:14 +0530

    17-01-2024

---
+++ 2024/01 - January/17-01-2024.cpp
@@ -0,0 +1,36 @@
+/*
+
+Author : Manas Rawat
+Date : 17/01/2024
+Problem : Unique Number of Occurrences
+Difficulty : Easy
+Problem Link : https://leetcode.com/problems/unique-number-of-occurrences/description/
+Video Solution : https://leetcode.com/problems/unique-number-of-occurrences/
+
+*/
+
+
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        vector<int> f(1001, 0);
+        int count = 1;
+
+        for(int i = 1; i < arr.size(); i++){
+            if(arr[i] != arr[i - 1]){
+                if(f[count])
+                    return 0;
+
+                f[count] = 1;
+                count = 1;
+            }
+            else{
+                ++count;
+            }
+        }
+
+        return !f[count];
+    }
+};
