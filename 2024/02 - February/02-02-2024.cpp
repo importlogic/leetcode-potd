b2d69e41af1b294a4eb660afaa62b87fa5a4ead7
Manas Rawat <manasrawat.cse@gmail.com>
2024-02-02 06:43:47 +0530

    02-02-2024

---
+++ 2024/02 - February/02-02-2024.cpp
@@ -0,0 +1,39 @@
+/*
+
+Author : Manas Rawat
+Date : 02/02/2024
+Problem : https://leetcode.com/problems/sequential-digits/description/
+Difficulty : Medium
+Problem Link : https://leetcode.com/problems/sequential-digits/description/
+Video Solution : https://youtu.be/RYLA6hDQ7lA
+
+*/
+
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) {
+        vector<int> ans;
+
+        for(int start = 1; start < 10; start++){
+            int cur = start + 1;
+            int val = start;
+
+            if(val >= low and val <= high)
+                ans.push_back(val);
+
+            for(; cur < 10; cur++){
+                val *= 10;
+                val += cur;
+
+
+                if(val >= low and val <= high)
+                    ans.push_back(val);
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
