8f1d388600c099210a1c2ac5513b1100fcce44ea
Manas Rawat <manasrawat.cse@gmail.com>
2024-02-12 07:10:02 +0530

    12-02-2024

---
+++ 2024/02 - February/12-02-2024.cpp
@@ -0,0 +1,29 @@
+/*
+
+Author : Manas Rawat
+Date : 12/02/2024
+Problem : Majority Element
+Problem Link : https://leetcode.com/problems/majority-element/description/
+Video Solution : https://youtu.be/CDBFHIhjHBU
+
+*/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> f;
+
+        for(auto i : nums){
+            ++f[i];
+        }
+
+        for(auto i : f){
+            if(i.second > nums.size() / 2){
+                return i.first;
+            }
+        }
+
+        return 0;
+    }
+};f
