d575faa9556e2a3396414d724b7e16ea0b7ce705
Saransh Bangar <saranshbangad@gmail.com>
2024-03-21 07:34:44 +0530

    Create 21-03-2024.cpp

---
+++ 2024/03 - March/21-03-2024.cpp
@@ -0,0 +1,42 @@
+/*
+
+Author : Manas Rawat 
+Date : 20/03/2024
+Problem : Merge In Between Linked Lists
+Difficulty : Medium 
+Problem Link : https://leetcode.com/problems/merge-in-between-linked-lists/description
+Video Solution : NA
+
+*/
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        vector<int>help;
+        ListNode* temp=head;
+        while (temp!=nullptr)
+        {
+            help.push_back(temp->val);
+            temp=temp->next;
+        }
+        temp=head;
+        for (int i=help.size()-1;i>=0;i--)
+        {
+            temp->val=help[i];
+            temp=temp->next;
+        }
+        return head;
+    }
+};
